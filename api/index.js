49a76cd8d59808f269786014564333082fe88547
Avadhut <avadhut24jadhav@gmail.com>
2025-07-16 01:33:44 +0530

    added vercel origin

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, // Allows cookies and credentials
